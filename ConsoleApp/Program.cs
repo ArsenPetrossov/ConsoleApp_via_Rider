c337ac9b53b5a3cd23d45b75d1ef0a37e5c315d2
Mahmudi <@ArsenPetrossov>
2024-01-28 16:39:58 +0500

    feat: create simple calculator

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,3 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Первое число.");
+            var firstNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Математическая операция:\n +\n -\n *\n /\n");
+            var operation = Console.ReadLine();
+
+            Console.WriteLine("Второе число.");
+            var secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            double result;
+            switch (operation)
+            {
+                case "+":
+                    result = firstNumber + secondNumber;
+                    Console.WriteLine("Результат: " + result);
+                    break;
+                case "-":
+                    result = firstNumber - secondNumber;
+                    Console.WriteLine("Результат: " + result);
+                    break;
+                case "*":
+                    result = firstNumber * secondNumber;
+                    Console.WriteLine("Результат: " + result);
+                    break;
+                case "/":
+                    result = firstNumber / secondNumber;
+                    Console.WriteLine("Результат: " + result);
+                    break;
+                default:
+                    Console.WriteLine("Выбрана неверная операция.");
+                    break;
+            }
+        }
+    }
+}
